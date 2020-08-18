3b773f13adc2be63e58082e36789f984f35bff89
shalgorepublic <shahid.hameed@algorepublic.com>
2020-08-18 16:06:46 +0500

    webview issue resolve in dashboard screen

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -178,9 +178,7 @@ class _DashboardState extends State<Dashboard> {
                             alignment: FractionalOffset.center,
                             child: CircularProgressIndicator(),
                           )
-                        : Container(
-                            color: Colors.transparent,
-                          ),
+                        : Container()
                   ],
                 ),
               )
